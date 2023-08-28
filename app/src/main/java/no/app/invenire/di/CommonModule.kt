368ab9ffc3d482b2f753b8a7497541a441df7552
Haakon Pettersen <haakon@datek.no>
2023-08-28 18:53:02 +0200

    Define DI-module

---
+++ app/src/main/java/no/app/invenire/di/CommonModule.kt
@@ -0,0 +1,11 @@
+package no.app.invenire.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CommonModule {
+
+}
