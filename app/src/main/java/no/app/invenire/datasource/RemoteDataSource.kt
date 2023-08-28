b069063c8f069b2d506f17865e9fbeb82a00323d
Haakon Pettersen <haakon@datek.no>
2023-08-28 19:05:31 +0200

    Add data-source with request to get ads

---
+++ app/src/main/java/no/app/invenire/datasource/RemoteDataSource.kt
@@ -0,0 +1,11 @@
+package no.app.invenire.datasource
+
+import no.app.invenire.domain.AdResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface RemoteDataSource {
+
+    @GET("6a1bcc8f429dcdb8f9196e917e5138bd/raw/discover.json")
+    suspend fun getAds(): Response<AdResponse>
+}
