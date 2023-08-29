651725c2c5ffa59b4397aacc004e7ca352597d34
Haakon Pettersen <haakon@datek.no>
2023-08-29 15:48:50 +0200

    Create AdItemUI and mapper

---
+++ app/src/main/java/no/app/invenire/ui/models/AdItemUI.kt
@@ -0,0 +1,26 @@
+package no.app.invenire.ui.models
+
+import no.app.invenire.domain.AdItem
+import no.app.invenire.domain.AdType
+import no.app.invenire.util.NumberFormatter
+
+data class AdItemUI(
+    val description: String?,
+    val id: String,
+    val adType: AdType,
+    val price: String?,
+    val location: String?,
+    val imageUrl: String?,
+    val isFavorite: Boolean,
+)
+
+fun AdItem.toUiModel(): AdItemUI =
+    AdItemUI(
+        description = description,
+        id = id,
+        adType = adType,
+        price = price?.value?.let { NumberFormatter.formatWithSpaces(it) },
+        location = location,
+        imageUrl = image?.url?.let { "https://images.finncdn.no/dynamic/480x360c/$it" },
+        isFavorite = false
+    )
