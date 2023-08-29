d29874fda414f54b6b9f08cfc5f18783c5f3b640
Haakon Pettersen <haakon@datek.no>
2023-08-29 20:05:41 +0200

    Provide Room-database in Hilt injection

--- app/src/main/java/no/app/invenire/di/CommonModule.kt
+++ app/src/main/java/no/app/invenire/di/CommonModule.kt
@@ -1,9 +1,13 @@
 package no.app.invenire.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import no.app.invenire.database.AppDatabase
 import no.app.invenire.datasource.RemoteDataSource
 import no.app.invenire.datasource.RetrofitClientProvider
 
@@ -12,4 +16,11 @@ import no.app.invenire.datasource.RetrofitClientProvider
 class CommonModule {
     @Provides
     fun provideRemoteDataSource(): RemoteDataSource = RetrofitClientProvider.client
+
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase = Room.databaseBuilder(
+        context,
+        AppDatabase::class.java,
+        "ad_database"
+    ).build()
 }
