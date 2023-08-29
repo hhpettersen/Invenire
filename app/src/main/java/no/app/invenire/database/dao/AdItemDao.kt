82b398c15e5f58f15f7c74e6072131c65880a94e
Haakon Pettersen <haakon@datek.no>
2023-08-29 20:04:44 +0200

    Declare AdItemDao

---
+++ app/src/main/java/no/app/invenire/database/dao/AdItemDao.kt
@@ -0,0 +1,22 @@
+package no.app.invenire.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import no.app.invenire.ui.models.cache.AdItemEntity
+
+@Dao
+interface AdItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(adItem: AdItemEntity)
+
+    @Query("DELETE FROM ad_item WHERE id = :itemId")
+    suspend fun deleteById(itemId: String)
+
+    @Query("SELECT * FROM ad_item")
+    suspend fun getAll(): List<AdItemEntity>
+
+    @Query("DELETE FROM ad_item")
+    suspend fun clearAll()
+}
