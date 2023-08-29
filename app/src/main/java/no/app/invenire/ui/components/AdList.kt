06517c2d0d42a67bc75fcfbc54bc2d1c24feb922
Haakon Pettersen <haakon@datek.no>
2023-08-29 17:25:47 +0200

    Add composable to display list of ads

---
+++ app/src/main/java/no/app/invenire/ui/components/AdList.kt
@@ -0,0 +1,78 @@
+package no.app.invenire.ui.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import no.app.invenire.domain.AdType
+import no.app.invenire.ui.models.AdItemUI
+import no.app.invenire.ui.models.Ads
+import no.app.invenire.ui.theme.InvenireTheme
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun AdList(
+    modifier: Modifier = Modifier,
+    ads: Ads,
+    onHeartClicked: (AdItemUI) -> Unit,
+) {
+    LazyVerticalStaggeredGrid(
+        modifier = modifier,
+        columns = StaggeredGridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(vertical = 16.dp),
+        verticalItemSpacing = 32.dp,
+    ) {
+        items(ads.size) { index ->
+            AdCard(
+                modifier = Modifier,
+                ad = ads[index],
+                onHeartClicked = onHeartClicked,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewAdList() {
+    InvenireTheme {
+        AdList(
+            ads = listOf(
+                AdItemUI(
+                    description = "Some long description about the ad or something like that",
+                    id = "1",
+                    adType = AdType.BAP,
+                    price = "1000",
+                    location = "Oslo",
+                    imageUrl = "https://images.finncdn.no/dynamic/480x360c/2023/3/vertical-0/21/3/295/695/523_1539723838.jpg",
+                    isFavorite = false,
+                ),
+                AdItemUI(
+                    description = "Some long description about the ad or something like that",
+                    id = "1",
+                    adType = AdType.BAP,
+                    price = "1000",
+                    location = "Oslo",
+                    imageUrl = "https://images.finncdn.no/dynamic/480x360c/2023/3/vertical-0/21/3/295/695/523_1539723838.jpg",
+                    isFavorite = false,
+                ),
+                AdItemUI(
+                    description = "Some long description about the ad or something like that",
+                    id = "1",
+                    adType = AdType.BAP,
+                    price = "1000",
+                    location = "Oslo",
+                    imageUrl = "https://images.finncdn.no/dynamic/480x360c/2023/3/vertical-0/21/3/295/695/523_1539723838.jpg",
+                    isFavorite = true,
+                ),
+            ),
+            onHeartClicked = {},
+        )
+    }
+}
